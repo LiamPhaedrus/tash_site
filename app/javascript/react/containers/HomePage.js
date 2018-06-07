57a84c68a067a47695af8e7fdc3927a0c5b2635f
LiamPhaedrus <liamphaedrus@gmail.com>
2018-06-07 15:34:59 -0400

    React fetching data

--- app/javascript/react/containers/HomePage.js
+++ app/javascript/react/containers/HomePage.js
@@ -4,13 +4,41 @@ import React, { Component }  from 'react';
 class HomePage extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      kits: []
+    }
+    this.fetchKits = this.fetchKits.bind(this)
+  }
+
+  fetchKits () {
+    fetch('/api/v1/kits', {
+      credentials: 'same-origin',
+      method: 'GET'
+    })
+    .then(response=> response.json())
+    .then(parsed=> {
+      console.log(parsed)
+      this.setState({
+        kits: parsed
+      })
+    })
+  }
+
+  componentDidMount () {
+    this.fetchKits()
   }
 
   render () {
+    let kitNames = this.state.kits.map(kit => {
+      return (
+        <li>{kit.name}</li>
+      )
+    })
 
     return (
       <div>
         <h1>Yup yup yup</h1>
+        <ul>{kitNames}</ul>
       </div>
     )
   }
